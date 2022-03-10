f570933850cf3e0854ef0dcbe0af088dc4e9772a
Vũ Trường Giang <69238925+vutruonggiang-sys@users.noreply.github.com>
2022-03-10 15:24:52 +0700

    VTG main

--- app/src/main/java/com/rikkei/training/chat/MainActivity.java
+++ app/src/main/java/com/rikkei/training/chat/MainActivity.java
@@ -1,14 +1,42 @@
 package com.rikkei.training.chat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.navigation_friends:
+                        return true;
+                    case R.id.navigation_message:
+                        return true;
+                    case R.id.navigation_account:
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+        });
+    }
+    private void getFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).commit();
+    }
+    public void init(){
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
     }
 }
