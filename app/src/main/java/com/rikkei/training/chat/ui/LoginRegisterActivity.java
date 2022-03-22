d7ef3b7e700715655e66bae7727eb1f2f53089ee
Vi Nhật Giang <vinhatgiang@gmail.com>
2022-03-22 09:41:28 +0700

    Man hinh Login SignUp Slapsh

---
+++ app/src/main/java/com/rikkei/training/chat/ui/LoginRegisterActivity.java
@@ -0,0 +1,37 @@
+package com.rikkei.training.chat.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.rikkei.training.chat.R;
+
+public class LoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+        setFragment(new LoginFragment());
+    }
+
+    private void setFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).commit();
+    }
+
+    public void goToRegisterFragment() {
+        setFragment(new RegisterFragment());
+    }
+
+    public void goToLoginFragment() {
+        setFragment(new LoginFragment());
+    }
+
+    public void goToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
