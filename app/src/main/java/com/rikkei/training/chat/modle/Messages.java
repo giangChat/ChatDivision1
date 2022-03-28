cb0e1b31087b278220717e51ce05efd19873e93a
Vi Nhật Giang <vinhatgiang@gmail.com>
2022-03-28 14:03:38 +0700

    VNG Home Chat

---
+++ app/src/main/java/com/rikkei/training/chat/modle/Messages.java
@@ -0,0 +1,16 @@
+package com.rikkei.training.chat.modle;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class Messages {
+
+    private String idReceiver;
+    private String idSender;
+    private boolean checkSeen;
+    private String message;
+    private long timeLong;
+    private String type;
+
+
+}
---
+++ app/src/main/java/com/rikkei/training/chat/ui/ChatFragment.java
@@ -0,0 +1,6 @@
+package com.rikkei.training.chat.ui;
+
+import androidx.fragment.app.Fragment;
+
+public class ChatFragment extends Fragment {
+}
