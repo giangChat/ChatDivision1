9718388c46cb9816aa01bd8c3d5946860cc71605
Vi Nhật Giang <vinhatgiang@gmail.com>
2022-04-01 15:37:20 +0700

    VNG Sửa lại giao diện

--- app/src/main/java/com/rikkei/training/chat/ui/HomeChatactivity.java
+++ app/src/main/java/com/rikkei/training/chat/ui/HomeChatactivity.java
@@ -1,7 +1,22 @@
 package com.rikkei.training.chat.ui;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class HomeChatactivity extends Fragment {
+import com.rikkei.training.chat.R;
 
+
+public class HomeChatactivity extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home_chat, container, false);
+    }
 }
